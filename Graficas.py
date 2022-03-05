3e90baff86e774b368a60af46b486e921faaf812
UO263630 <uo263630@uniovi.es>
2022-03-05 17:15:39 +0100

    pruebas3

--- Graficas.py
+++ Graficas.py
@@ -44,25 +44,27 @@ def grafica1(grados,aux):
         plt.savefig("WindDirection2.png",bbox_inches="tight")
     #plt.show()
 
-    
+
 
 
 def grafica2(grados,aux):
     
     x = np.linspace(1, 23, 12)
     #grados=[306.76, 306.55, 306.29, 306.45, 307.03, 320.28, 321.86, 311.77, 311.85, 310.8, 308.63, 310.16]
-    ejex=["01:00","03:00","05:00","07:00","09:00","11:00","13:00","15:00","17:00","19:00","21:00","23:00","00:00"]
+    ejex=["01:00","03:00","05:00","07:00","09:00","11:00","13:00","15:00","17:00","19:00","21:00","23:00"]
 
 
     fig = plt.figure()
     ax = fig.gca()
-    #print(x)
     #print(np.zeros(x.shape))
 
    
 
     a=[]
     i=0
+    print("--------------------------------------------")
+    print(grados)
+    print(x)
     while(i<len(grados)):
         b=-np.tan(grados[i])*x[i]
         a.append(1-b)
